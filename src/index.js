c64625f2ecfa164be624c0afa2ff168b7bd9e2cc
Anton Korzunov <akorzunov@atlassian.com>
2017-12-01 00:10:05 +1100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,49 @@
+function generate(libraryName) {
+
+  let hasError = false;
+
+  function testFunction(a, b, file, name) {
+    if (a.length !== b.length) {
+      console.error(name + ': in' + file + '\n\t\t' + a.toString() + '\n\tdoes not match\n\t\t' + b.toString());
+      throw new Error(libraryName + ': function argument mismatch: ' + file + ': ' + name);
+    }
+  }
+
+  function test(a, b, file, name) {
+    if (!b) {
+      throw new Error(libraryName + ': mocked export "' + name + '" does not exists in ' + file);
+    }
+    const typeOfA = typeof a;
+    const typeOfB = typeof b;
+    if (typeOfA !== typeOfB) {
+      throw new Error(libraryName + ': exported type mismatch: ' + file + ':' + name + '. Expected ' + typeOfB + ', got ' + typeOfB + '');
+    }
+    if (typeOfA === 'function') {
+      return testFunction(a, b, file, name);
+    }
+  }
+
+  function matchExports(realExports, mockedExports, realFile, mockFile) {
+    hasError = false;
+    if (typeof mockedExports !== typeof realExports) {
+      throw new Error(libraryName + ': mock ' + mockFile + ' export does not match real file');
+    }
+    if (typeof mockedExports === 'function') {
+      test(mockedExports, realExports, realFile, 'exports');
+    } else if (typeof mockedExports === 'object') {
+      Object.keys(mockedExports).forEach(key => {
+        try {
+          test(mockedExports[key], realExports[key], realFile, key)
+        } catch (e) {
+          console.error(e.message, '\n');
+          hasError = true;
+        }
+      });
+    }
+    return hasError;
+  }
+
+  return matchExports;
+}
+
+module.exports = generate;
